7a2da1e97ea0bbcaca274cb7a28df10d3ecc08be
Abraham Gustafson <agbues@gmail.com>
2023-09-04 20:40:45 -0700

    Added the first bloodiest round analyzer. So far data suggests round 6, 9, 7, 3, 4

--- rummy/sandbox.py
+++ rummy/sandbox.py
@@ -1,3 +1,17 @@
+"""
+ToDo: 
+ - Build player filtering
+ - Get Christine's data
+ - Get Mom and Dad's data
+ - 
+
+Analysis ideas
+ [ ] See which rounds are the bloodiest
+ [ ] ^ But filtered by player count
+ [ ] Probability of winning a game over time
+ [ ] Who has the best relative end score (average points +/- relative to opponents)
+"""
+
 import argparse
 import os
 import csv
@@ -9,20 +23,45 @@ from datetime import datetime
 import itertools
 
 
-DEBUG_ENABLED = True
-
+DEBUG_ENABLED = False
+ROUNDS = 11
+ROUND_OFFSET = 3
 
 class Game:
     """Data class for holding a single Game"""
 
-    def __init__(self, user_to_score_map, date, round):
+    def __init__(self, user_to_score_map, file_name):
         """
         Args:
            user_to_score_map: map<string, list(score)>
         """
         self.user_to_score_map = user_to_score_map
-        self.date = date
-        self.round = round
+        self.file_name = file_name
+        print("loading:   [{}]".format(self.file_name))
+        self._validate()
+        print("validated: [{}]".format(self.file_name))
+
+    def _validate(self):
+        assert len(self.user_to_score_map) > 0
+        for _, scores in self.user_to_score_map.items():
+            if(DEBUG_ENABLED):
+                print("File: {}, {}/{}".format(self.file_name, len(scores), ROUNDS))
+            assert len(scores) == ROUNDS
+
+    def player_count(self):
+        return len(self.user_to_score_map)
+
+    def players(self):
+        return self.user_to_score_map.keys()
+
+    def scores(self):
+        return self.user_to_score_map
+
+    def score_array(self):
+        """
+        Return a 2d array of scores
+        """
+        pass
 
     # map<string, list(score)>
     user_to_score_map = dict()
@@ -47,17 +86,77 @@ def load_game(file_path):
         for row in csv_reader:
             if is_first_row:
                 is_first_row = False
-                print(row)
                 for idx, person in enumerate(row):
                     user_to_score_map[person] = list()
                     user_order_map[idx] = person
                 continue
             
             for idx, score in enumerate(row):
-                user_to_score_map[user_order_map[idx]].append(score)
+                user_to_score_map[user_order_map[idx]].append(int(score))
 
-        game = Game(user_to_score_map, 0, 0)
+        game = Game(user_to_score_map, file_path)
+
     return game
+
+def bloodiest_round_stats(games, player_count_filter=0):
+    """
+    Print out statistics by round for the average point differential.
+    Going to divide by player count for now
+
+    Args:
+        games(list(Game)): list of games to analyze.
+    """
+    overall_round_score_change = [0] * ROUNDS
+    overall_round_score_change_abs = [0] * ROUNDS
+
+    for game in games:
+        if (player_count_filter > 0 and game.player_count() != player_count_filter):
+            continue
+
+        round_score_change = [0] * ROUNDS
+        round_score_change_abs = [0] * ROUNDS
+    
+        last_player_score = [0] * game.player_count()
+        
+        if (DEBUG_ENABLED):
+            print("Game start ----")
+
+        # Safe due to Game::_validate()
+        for round in range(0, ROUNDS):
+            this_round_score = [game.scores()[player][round] for player in game.players()]
+
+            deviation_by_player = [0] * game.player_count()
+            total_round_score_diff = 0
+            total_round_score_diff_abs = 0
+
+            for player_idx, score in enumerate(this_round_score):
+                dev = score - last_player_score[player_idx]
+                deviation_by_player[player_idx] = dev
+                total_round_score_diff += dev
+                total_round_score_diff_abs += abs(dev)
+            
+            if (total_round_score_diff != 0):
+                round_score_change[round] = total_round_score_diff / game.player_count()
+                overall_round_score_change[round] += total_round_score_diff / game.player_count()
+            if (total_round_score_diff_abs != 0):
+                round_score_change_abs[round] = total_round_score_diff_abs / game.player_count()
+                overall_round_score_change_abs[round] += total_round_score_diff_abs / game.player_count()
+            last_player_score = this_round_score
+
+            if (DEBUG_ENABLED):
+                print("[{}] - round_score_change: {}".format(round + ROUND_OFFSET, round_score_change[round]))
+                print("[{}] - round_score_change_abs: {}".format(round + ROUND_OFFSET, round_score_change_abs[round]))
+        
+        if (DEBUG_ENABLED):
+            print("Game end   ----")
+
+    for round in range(0, ROUNDS):
+        if (overall_round_score_change[round] != 0):
+            print("[{}] - overall_round_score_change: {}".format(round + ROUND_OFFSET, overall_round_score_change[round] / len(games)))
+        
+        # if (overall_round_score_change_abs[round] != 0):
+        #     print("[{}] - overall_round_score_change_abs: {}".format(round + ROUND_OFFSET, overall_round_score_change_abs[round] / len(games)))
+
 
 def load_all_games():
     datasets_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), "datasets")
@@ -66,26 +165,29 @@ def load_all_games():
     games = []
     for game_file in game_files:
         games.append(load_game(os.path.join(datasets_dir, game_file)))
-    print(games)
+    return games
 
-def run_script(game_csv):
+def run_script():
     """
     Args:
         game_csv(string): String file assumed to be in datasets dir
     """
-    full_filepath = os.path.join(os.path.dirname(os.path.abspath(__file__)), "datasets", game_csv)
-    game = load_game(full_filepath)
+    games = load_all_games()
+    bloodiest_round_stats(games)
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Test playground')
-    parser.add_argument('--game_csv', metavar='[GAME.csv]', type=str,
-                        help='Name of file in DrinkingGames/rummy/datasets/')
+    # parser.add_argument('--game_csv', metavar='[GAME.csv]', type=str,
+    #                     help='Name of file in DrinkingGames/rummy/datasets/')
     args = parser.parse_args()
     print(args)
 
-    load_all_games()
+    # load_all_games()
     # run_script(**vars(args))
+    run_script()
 
 
 
 # BUGS:
+
